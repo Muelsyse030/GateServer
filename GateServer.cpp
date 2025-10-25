705357513a3f6455eb5a9fea52b77c6e25755866
Muelsyse030 <15279290607@163.com>
2025-10-25 18:29:04 +0800

    配置redis服务

--- GateServer.cpp
+++ GateServer.cpp
@@ -1,8 +1,36 @@
 #include "const.h"
 #include "CServer.h"
 #include "ConfigMgr.h" 
+#include <cassert>
+#include <hiredis/hiredis.h>
+#include <assert.h>
+#include "RedisMgr.h"
+
+void TestRedisMgr() {
+    assert(RedisMgr::GetInstance()->Connect("127.0.0.1", 6380));
+    assert(RedisMgr::GetInstance()->Auth("123456"));
+    assert(RedisMgr::GetInstance()->Set("blogwebsite", "llfc.club"));
+    std::string value = "";
+    assert(RedisMgr::GetInstance()->Get("blogwebsite", value));
+    assert(RedisMgr::GetInstance()->Get("nonekey", value) == false);
+    assert(RedisMgr::GetInstance()->HSet("bloginfo", "blogwebsite", "llfc.club"));
+    assert(RedisMgr::GetInstance()->HGet("bloginfo", "blogwebsite") != "");
+    assert(RedisMgr::GetInstance()->ExistsKey("bloginfo"));
+    assert(RedisMgr::GetInstance()->Del("bloginfo"));
+    assert(RedisMgr::GetInstance()->Del("bloginfo"));
+    assert(RedisMgr::GetInstance()->ExistsKey("bloginfo") == false);
+    assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue1"));
+    assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue2"));
+    assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue3"));
+    assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+    assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+    assert(RedisMgr::GetInstance()->LPop("lpushkey1", value));
+    assert(RedisMgr::GetInstance()->LPop("lpushkey2", value) == false);
+    RedisMgr::GetInstance()->Close();
+}
 
 int main(int argc, char* argv[]) {
+    TestRedisMgr();
     auto & gCfgMgr = ConfigMgr::GetInstance();
     std::string gate_port_str = gCfgMgr["GateServer"]["port"];
     unsigned short gate_port = atoi(gate_port_str.c_str());
