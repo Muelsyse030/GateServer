5ec93cd3712138b09a8282f51d23c043d4409d4c
Muelsyse030 <15279290607@163.com>
2025-10-27 23:58:12 +0800

    完美解决bug

--- CServer.cpp
+++ CServer.cpp
@@ -7,20 +7,27 @@ CServer::CServer(boost::asio::io_context& ioc, unsigned short& port) :
 	
 }
 void CServer::Start() {
-	auto self = shared_from_this();
-	auto& io_context = AsioIOServicePool::GetInstance()->GetIOService();
-	std::shared_ptr<HttpConnection> new_con = std::make_shared<HttpConnection>(io_context);
-	_acceptor.async_accept(new_con->GetSocket(), [self , new_con](beast::error_code ec) {
-		try {
-			//出错放弃链接 继续监听其他链接
-			if (ec) {
-				self->Start();
-				return;
-			}
-			new_con->Start();
-		}
-		catch (std::exception & exp) {
-		
-			}
-		});
+    auto self = shared_from_this();
+    auto& io_context = AsioIOServicePool::GetInstance()->GetIOService();
+    std::shared_ptr<HttpConnection> new_con = std::make_shared<HttpConnection>(io_context);
+
+    _acceptor.async_accept(new_con->GetSocket(), [self, new_con](beast::error_code ec) {
+        try {
+            if (!ec) {
+                std::cout << "[INFO] New client connected from: "
+                    << new_con->GetSocket().remote_endpoint() << std::endl;
+                new_con->Start();
+            }
+            else {
+                std::cerr << "[ERROR] Accept failed: " << ec.message() << std::endl;
+            }
+        }
+        catch (std::exception& exp) {
+            std::cerr << "[EXCEPTION] " << exp.what() << std::endl;
+        }
+
+        
+        self->Start();
+        });
 }
+
--- RedisMgr.cpp
+++ RedisMgr.cpp
@@ -4,9 +4,9 @@
 
 RedisMgr::RedisMgr() {
 	auto& gCfgMgr = ConfigMgr::GetInstance();
-	auto host = gCfgMgr["Redis"]["Host"];
-	auto port = gCfgMgr["Redis"]["Port"];
-	auto pwd = gCfgMgr["Redis"]["Passwd"];
+	auto host = gCfgMgr["Redis"]["host"];
+	auto port = gCfgMgr["Redis"]["port"];
+	auto pwd = gCfgMgr["Redis"]["passwd"];
 	_con_pool.reset(new RedisConPool(5, host.c_str(), atoi(port.c_str()), pwd.c_str()));
 }
 
